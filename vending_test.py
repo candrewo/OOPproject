71f64dc2cf73f9caa88b33bf21bf7c243b595958
Andrew <caolegario@gmail.com>
2014-02-19 17:28:39 -0800

    added tests for all methods

--- vending_test.py
+++ vending_test.py
@@ -10,6 +10,24 @@ class TestVendingMachine(unittest.TestCase):
 		self.oj = NewItem('oj', 3)
 		self.stock = {'coca-cola': 10, 'sprite': 10,'root beer': 10}
 		
-	def test_sell (self):
-		self.vending.sell(self.stock['sprite'])
-		self.assertEqual(self.stock['sprite'],9)
+	def get_stock_test (self):
+		self.assertEqual(self.vending.get_stock(),{'coca-cola': 10,
+		 'sprite': 10,'root beer': 10})
+
+	def replenish_test(self):
+		self.vending.replenish('sprite',20)
+		self.assertEqual(self.stock,{'coca-cola': 10,
+		 'sprite': 30,'root beer': 10})
+
+	def sell(self):
+		self.vending.sell(sprite)
+		self.assertEqual(self.stock,{'coca-cola': 10,
+		 'sprite': 10,'root beer': 10})
+
+	def add_new_item(self):
+		self.vending.add_new_item(oj.item, oj.amount)
+		self.assertEqual(self.stock,{'coca-cola': 10, 'sprite': 10,
+			'root beer': 10, 'oj': 3})
+
+if __name__ == '__main__':
+	unittest.main()
