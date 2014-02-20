e3e93be8f50c6b38093ac2a9ee0ff9bac053d483
Andrew <caolegario@gmail.com>
2014-02-19 17:11:14 -0800

    2nd

--- vending_machine.py
+++ vending_machine.py
@@ -1,14 +1,48 @@
 class VendingMachine(object):
-	def __init__ (self, serial_num):
-		self.serial_num = serial
-		stock = {
-            'coca-cola': 0,
-            'sprite': 1,
-            'fanta': 0,
-        }
-	def sale(self, item):
-		self.stock = item
-		if stock[item] > 0:
-			stock[item] -= 1
-		else:
-			raise Exception('empty')
+
+    # class variables
+    stock = {'coca-cola': 10, 'sprite': 10,'root beer': 10}
+
+    # instance variables
+    def __init__ (self, serial_num):
+        self.serial_num = serial_num
+
+    def sell(self, item): #method: sell items
+        if self.stock[item] > 0:
+            self.stock[item] -= 1
+            return self.stock
+        else:
+            raise Exception('empty')
+
+    def replenish(self, item, number): #method: restock the vending machine
+        self.stock[item] = number
+        return self.stock
+
+    def get_stock(self): #method: current stock of drinks
+        return self.stock
+
+    def add_new_item(self, new_item, amount):
+    	self.stock[new_item] = amount
+    	return self.stock
+
+
+class NewItem(object):
+
+    def __init__(self, item,amount):
+        self.item = item
+        self.amount = amount
+
+
+
+# vending = VendingMachine(156700)
+# # oj = NewItem('oj',3)
+# print vending.sell('sprite')
+# # print vending.replenish('sprite', 20)
+# vending.sell('sprite')
+# vending.sell('sprite')
+# vending.sell('sprite')
+# vending.sell('sprite')
+# # vending.add_new_item(oj.item,oj.amount)
+# # vending.add_new_item('oj',3)
+# print vending.sell('sprite')
+# print vending.get_stock()
