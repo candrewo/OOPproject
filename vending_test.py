0ca8ee0c0d899e83bb1820c5622f7dcbbe9d394d
Andrew <caolegario@gmail.com>
2014-02-19 17:11:52 -0800

    2nd

--- vending_test.py
+++ vending_test.py
@@ -1,11 +1,15 @@
 import unittest
-from vending_machine import VendingMachine
+from vending_machine import VendingMachine, NewItem
 
 class TestVendingMachine(unittest.TestCase):
 
-	def setUp(self):
-		self.vend = VendingMachine('num1')
 
-	def test_sale (self):
-		self.vend.sale(sprite)
-		self.assertEqual (stock[item,0])
+
+	def setUp(self):
+		self.vending = VendingMachine(156700)
+		self.oj = NewItem('oj', 3)
+		self.stock = {'coca-cola': 10, 'sprite': 10,'root beer': 10}
+		
+	def test_sell (self):
+		self.vending.sell(self.stock['sprite'])
+		self.assertEqual(self.stock['sprite'],9)
