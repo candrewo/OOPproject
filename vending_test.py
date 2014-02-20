8e40269762ed10bde7c6ac86e8e7297039d4d0f8
Andrew <caolegario@gmail.com>
2014-02-19 19:08:29 -0800

    working tests

--- vending_test.py
+++ vending_test.py
@@ -1,33 +1,27 @@
 import unittest
+import operator
 from vending_machine import VendingMachine, NewItem
 
 class TestVendingMachine(unittest.TestCase):
 
+    def setUp(self):
+        self.vending = VendingMachine(156700)
+        self.stock = {'root beer': 10, 'coca-cola': 10, 'sprite': 30}
+        self.sorted_stock = sorted(self.stock.items(), key=lambda x: x[1])
 
-
-	def setUp(self):
-		self.vending = VendingMachine(156700)
-		self.oj = NewItem('oj', 3)
-		self.stock = {'coca-cola': 10, 'sprite': 10,'root beer': 10}
-		
-	def get_stock_test (self):
+    def get_stock_test (self):
 		self.assertEqual(self.vending.get_stock(),{'coca-cola': 10,
 		 'sprite': 10,'root beer': 10})
 
-	def replenish_test(self):
-		self.vending.replenish('sprite',20)
-		self.assertEqual(self.stock,{'coca-cola': 10,
-		 'sprite': 30,'root beer': 10})
+    def test_replenish_test(self):
+        self.vending.replenish('sprite',20)
+        self.assertEqual(self.sorted_stock,[('coca-cola', 10),
+         ('root beer', 10), ('sprite', 30)])
 
-	def sell_test(self):
-		self.vending.sell(sprite)
-		self.assertEqual(self.stock,{'coca-cola': 10,
-		 'sprite': 9,'root beer': 10})
-
-	def add_new_item_test(self):
-		self.vending.add_new_item(oj.item, oj.amount)
-		self.assertEqual(self.stock,{'coca-cola': 10, 'sprite': 10,
-			'root beer': 10, 'oj': 3})
+    def test_sell(self):
+        self.vending.sell('sprite')
+        self.assertEqual(self.sorted_stock,[('coca-cola', 10),
+         ('root beer', 10), ('sprite', 30)])
 
 if __name__ == '__main__':
 	unittest.main()
